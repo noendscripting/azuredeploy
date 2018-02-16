b563cfd77cced89739aef364e853463c0a6df4bf
Mike Resnick <miresnic@MIRESNICSP4>
2018-02-16 11:52:44 -0500

    path updates

--- WiNRM-SQL.ps1
+++ WiNRM-SQL.ps1
@@ -1,4 +1,4 @@
-$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $env:COMPUTERNAME
+Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $env:COMPUTERNAME
 Enable-PSRemoting -SkipNetworkProfileCheck -Force
 New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
 New-NetFirewallRule -DisplayName "Windows Remote Management (HTTPS-In)" -Name "Windows Remote Management (HTTPS-In)" -Profile Any -LocalPort 5986 -Protocol TCP
@@ -9,8 +9,8 @@ try
   #Verify if PowerShellGet module is installed. If not install
   if (!(Get-Module -Name PowerShellGet))
   {
-      Invoke-WebRequest 'https://download.microsoft.com/download/C/4/1/C41378D4-7F41-4BBE-9D0D-0E4F98585C61/PackageManagement_x64.msi' -OutFile "C:\Packages\PackageManagement_x64.msi"
-      Start-Process "C:\Packages\PackageManagement_x64.msi" -ArgumentList "/qn" -Wait
+      Invoke-WebRequest 'https://download.microsoft.com/download/C/4/1/C41378D4-7F41-4BBE-9D0D-0E4F98585C61/PackageManagement_x64.msi' -OutFile "$($PWD)\PackageManagement_x64.msi"
+      Start-Process "$($PWD)\PackageManagement_x64.msi" -ArgumentList "/qn" -Wait
   }
     
   ##Verify if PSWindowsUpdate PowerShell Module is installed. If not install.
@@ -19,10 +19,11 @@ try
    Install-Module -Name SQLServerDSC,StorageDSC,XtimeZone,PSDscResources -Scope AllUsers -Confirm:$false -Force
   #  }
   #  Get-WUInstall -WindowsUpdate -AcceptAll -AutoReboot -Confirm:$FALSE -ErrorAction stop
-Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=839516" -OutFile "C:\Packages\wmf5.1.msu"
-Start-Process -FilePath 'wusa.exe' -ArgumentList "C:\Packages\wmf5.1.msu /quiet /noreboot" -NoNewWindow -Wait
+Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=839516" -OutFile "$($PWD)\wmf5.1.msu"
+Start-Process -FilePath 'wusa.exe' -ArgumentList "$($PWD)\wmf5.1.msu /quiet /noreboot" -NoNewWindow -Wait
 }
 catch
 {
     Write-Output "Oops. Something failed"
 }
+ $PWD
