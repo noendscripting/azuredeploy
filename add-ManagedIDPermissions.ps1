bfc0f4b405185e6a5cb724aa9ea65148a6b3db48
noendscripting <miresnic@microsoft.com>
2021-06-24 19:05:45 -0400

    managed ID script

---
+++ add-ManagedIDPermissions.ps1
@@ -0,0 +1,21 @@
+param(
+   
+    [Parameter(Mandatory=$true)]
+    [string]$targetAppObjId,
+    [Parameter(Mandatory=$true)]
+    [string[]]$permissions,
+    [string]$resourceAppId = '00000003-0000-0000-c000-000000000000'
+)
+
+Connect-MgGraph -Scopes Application.ReadWrite.All
+
+$resourceAppObjId = (Get-MgServicePrincipal -Filter "appid eq '$($resourceAppId)'").Id
+
+
+
+foreach($permission in $permissions)
+{
+    $permissioinId = ((Get-MgServicePrincipal -ServicePrincipalId $resourceAppObjId  | Select-Object -ExpandProperty AppRoles | Group-Object Value -AsHashTable -AsString)[$permission]).id
+    New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $targetAppObjId -AppRoleId $permissioinId -PrincipalId $targetAppObjId -ResourceId $resourceAppObjId
+    Clear-Variable permissioinId
+}
